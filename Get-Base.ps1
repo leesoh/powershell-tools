3e26ac2e4932c0e5e4e81a7c1160cca84b7220f2
li-so <liam@liso.ca>
2014-11-03 18:31:55 -0700

    Initial commit

---
+++ Get-Base.ps1
@@ -0,0 +1,61 @@
+<#
+.SYNOPSIS
+	PowerShell script to encode text to Base64 and back again.
+.NOTES
+    Author: Liam Somerville
+.EXAMPLE
+    Get-Base -String 'Hello World!'
+.EXAMPLE
+    Get-Base -Base64 'SGVsbG8gV29ybGQh'
+#>
+
+[CmdletBinding()]
+param(
+    [parameter(Mandatory=$false)]
+    [string]$Base64,
+    [parameter(Mandatory=$false)]
+    [string]$String
+    )
+
+function Write-Header
+{
+    param ($Title)
+    $Gap = '  '
+    $Title = $Gap + $Title + $Gap
+    $Open = '['
+    $Spacer = '='*10
+    $Close = ']'
+    
+    Write-Host
+    $Open + $Spacer + $Title + $Spacer + $Close
+    Write-Host
+} # end Write-Header
+
+
+
+function ConvertTo-Base
+{
+    param ($String)
+    $Base64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($String))
+    $Base64
+} # end ConvertTo-Base
+
+function ConvertFrom-Base
+{
+    param ($Base64)
+    $String = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Base64))
+    $String
+} # end ConvertFrom-Base
+
+Write-Header('Get-Base v0.1')
+
+if ($Base64)
+{
+    Write-Host $Base64 '>>>' $(ConvertFrom-Base($Base64))
+} # end if
+
+elseif ($String)
+{
+    Write-Host $String '>>>' $(ConvertTo-Base($String))
+} # end elseif
+Write-Host
