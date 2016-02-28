d83f14c0e6597dbf0f97d2a9fedee580fa4a1e0c
leesoh <liam@liso.ca>
2016-02-27 22:04:07 -0700

    Initial commit

---
+++ Get-Substring.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+    Returns values between to strings.
+
+.DESCRIPTION
+    Takes start and end character, returns the middle.
+
+.PARAMETER Start
+    Start string.
+
+.PARAMETER End
+    End string.
+
+.PARAMETER Path
+    Path to file
+
+.EXAMPLE
+    Get-Substring -Start 'Hello' -End 'Peanut' -Path C:\Path\To\File
+#>
+
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $True,
+               ValueFromPipeline = $True)]
+    [string]$Start,
+
+    [Parameter(Mandatory = $True,
+               ValueFromPipeline = $True)]
+    [string]$End,
+
+    [Parameter(Mandatory = $True,
+               ValueFromPipeline = $True)]
+    [string]$Path
+)
+
+# Need to get multi-line working
+[regex]$Pattern = "(?<=$Start).*?(?=$End)"
+$Results = Select-String -Path $Path -Pattern $Pattern
+$Results.Matches[0].Value
