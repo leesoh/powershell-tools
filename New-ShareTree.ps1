530cd43ae69906e7259c8237e473309d0b0718e1
leesoh <liam@liso.ca>
2016-04-15 11:16:23 -0600

    Add file creation

--- New-ShareTree.ps1
+++ New-ShareTree.ps1
@@ -15,7 +15,7 @@
 .PARAMETER Width
     Number of folders to create at each level. Defaults to five.
 
-.PARAMETER Files
+.PARAMETER FileCount
     Number of files to create in each folder.
 
 .EXAMPLE
@@ -41,8 +41,27 @@ Param (
     $Width = 5,
     [ValidateRange(1,15)]
     [int]
-    $Files = 5
+    $FileCount = 5
 )
+
+function New-File {
+    param (
+        [Parameter(Mandatory = $True)]
+        [ValidateScript({Test-Path -Path $_})]
+        [string]
+        $Path,
+        [Parameter(Mandatory = $True)]
+        [string[]]
+        $FileNames,
+        [int]
+        $FileCount = 5
+    )
+    for ($i = 0; $i -lt $FileCount; $i++) {
+        $File = Get-Random -InputObject $FileNames
+        $FileNames = $FileNames | Where-Object {$_ -ne $File}
+        New-Item -Path $Path -ItemType File -Name "$File.txt"
+    }
+}
 
 function New-WideFolders {
     param (
@@ -52,10 +71,10 @@ function New-WideFolders {
         $Path,
         [Parameter(Mandatory = $True)]
         [string[]]
-        $FolderNames#,
-        #[Parameter(Mandatory = $True)]
-        #[ValidateRange(1,15)]
-        #$Width
+        $FolderNames,
+        [Parameter(Mandatory = $True)]
+        [ValidateRange(1,15)]
+        $Width
     )
     for ($i = 0; $i -lt $Width; $i++) {
         $Folder = Get-Random -InputObject $FolderNames
@@ -73,8 +92,8 @@ function New-Folders {
         $Path
     )
     Write-Verbose -Message "Now at $Depth levels deep ($Path)"
-    New-WideFolders -Path $Path -FolderNames $FolderNames
-    # Probably make files here
+    New-WideFolders -Path $Path -FolderNames $FolderNames -Width $Width
+    New-File -Path $Path -FileNames $FileNames -FileCount 5
     if ($Depth -gt 0) {
         Write-Verbose -Message "Going deeper..."
         $Depth--
