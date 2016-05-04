502b6d560b3350ccc3cb0b36d1a3787d5ada8288
leesoh <liam@liso.ca>
2016-05-04 17:12:06 -0600

    Refactor for multi-target

--- Get-NetInfo.ps1
+++ Get-NetInfo.ps1
@@ -7,76 +7,73 @@
         Gathers IP Addresses, Subnet Mask, and Default Gateway for each adapter present on the target and returns
         an array.
 
-        .PARAMETER $Target
-        The target of the query.
+        .PARAMETER ComputerName
+        Target computer(s)
 
         .EXAMPLE
-        Get-NetInfo -Target "192.168.1.1"
-
-        .NOTES
-        Version: 1.0
-        Author: Liam Somerville
-        Date: 2015-10-21
     #>
 
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory = $True,
                    ValueFromPipeLine = $True)]
-        $Target,
-
-        $Credentials
+        [string[]]
+        $ComputerName
     )
 
     begin {
-        Write-Verbose "[*] Beginning Get-NetInfo for $Target..."
         $Results = @()
         $Namespace = "root\cimv2"
+        $Credentials = Get-Credential
     }
 
     process {
-        foreach ($T in $Target) {
+        foreach ($C in $ComputerName) {
+            Write-Verbose "[*] Beginning Get-NetInfo for $C..."
+            try {
+                $WMINetInfo = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $C -Namespace $Namespace -Credential $Credentials -ErrorAction Stop
+                $WMIEthInfo = Get-WmiObject -Class Win32_NetworkAdapter -ComputerName $C -Namespace $Namespace -Credential $Credentials -ErrorAction Stop
+                $WMISysInfo = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $C -Namespace $Namespace -Credential $Credentials -ErrorAction Stop
+            }
 
-            Write-Verbose "[*] Gathering network information..."
-            $WMINetInfo = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $Target -Namespace $Namespace -Credential $Credentials
-            $WMIEthInfo = Get-WmiObject -Class Win32_NetworkAdapter -ComputerName $Target -Namespace $Namespace -Credential $Credentials
+            catch {
+                Write-Verbose -Message "[-] WMI information not available on $C"
+            }
 
             if ($WMINetInfo) {
                 Write-Verbose '[+] $WMINetInfo is present...'
                 foreach ($Adapter in $WMINetInfo) {
                     $AdapterName = "Adapter $($Adapter.Index)"
+                    Write-Verbose -Message "[*] Now processing $AdapterName"
 
                     # Get the index of the current adapter in the list of adapters. This allows us to correlate a MAC.
                     $AdapterIndex = [array]::IndexOf($WMINetInfo, $Adapter)
 
                     # We only care about this info if there is an IP address
                     if ($Adapter.IPAddress){
-                        Write-Verbose "[*] Creating information from $AdapterName..."
-                        $AdapterName = @{}
-                        $AdapterName['IPAddress'] = $Adapter.IPAddress[0]
-                        $AdapterName['SubnetMask'] = $Adapter.IPSubnet[0]
-                        $AdapterName['MACAddress'] = $WMIEthInfo[$AdapterIndex].MACAddress
+                        Write-Verbose -Message "[+] IP is present on $AdapterName"
+                        $AdapterProps = [ordered]@{
+                            'Computer' = $WMISysInfo.Name
+                            'Adapter' = $AdapterName
+                            'IPAddress' = $Adapter.IPAddress[0]
+                            'MAC' = $WMIEthInfo[$AdapterIndex].MACAddress
+                            'Subnet' = $Adapter.IPSubnet[0]
+                            'DefaultGateway' = 'N/A'
+                        }
 
                         # If there's also default gateway, record it
                         if ($Adapter.DefaultIPGateway) {
-                            $AdapterName['DefaultGateway'] = $Adapter.DefaultIPGateway[0]
+                            Write-Verbose -Message "[+] Default Gateway found on $AdapterName"
+                            $AdapterProps['DefaultGateway'] = $Adapter.DefaultIPGateway[0]
                         }
 
-                        # If there's no gateway, just put N/A
-                        elseif (!($Adapter.DefaultIPGateway)) {
-                            $AdapterName['DefaultGateway'] = 'N/A'
-                        }
-
-                        else {
-                            Write-Verbose "[-] No Default Gateway found for $AdapterName"
-                        }
-                        $Results += $AdapterName
+                        $AdapterInfo = New-Object -TypeName PSCustomObject -Property $AdapterProps
+                        $Results += $AdapterInfo
                     }
                 }
             }
             else {
-                Write-Error '[-] Network Information unavailable!'
-                $Results = 'Unavailable'
+                Write-Verbose -Message "[-] Network Information unavailable for $C"
             }
         }
     }
